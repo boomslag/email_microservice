89e834afefbb71c2271188394b73f6d743c2df60
SoloPython <alexander.eric.3@gmail.com>
2023-04-08 09:10:26 -0500

    Initial Commit

---
+++ consumer.py
@@ -0,0 +1,32 @@
+import os, django
+from confluent_kafka import Consumer
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+django.setup()
+
+from django.apps import apps
+
+consumer = Consumer({
+    'bootstrap.servers': os.environ.get('KAFKA_BOOTSTRAP_SERVER'),
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': os.environ.get('KAFKA_USERNAME'),
+    'sasl.password': os.environ.get('KAFKA_PASSWORD'),
+    'sasl.mechanism': 'PLAIN',
+    'group.id': 'email_group',
+    'auto.offset.reset': 'earliest'
+})
+consumer.subscribe(['email'])
+
+while True:
+    msg = consumer.poll(1.0)
+
+    if msg is None:
+        continue
+    if msg.error():
+        print("Consumer error: {}".format(msg.error()))
+        continue
+
+    print("Received message: {}".format(msg.value()))
+
+
+consumer.close()
