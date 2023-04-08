1c503bd0ae53c5d5e68e39e05668ac331a59a653
SoloPython <alexander.eric.3@gmail.com>
2023-04-08 09:35:36 -0500

    build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-# Exit on error
-set -o errexit
+set -e
 
+# Create the build/static folder if it doesn't exist
+mkdir -p build/static
+
+# Continue with the rest of the build script
+python -m venv .venv
+source .venv/bin/activate
+pip install --upgrade pip
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
-
-# python manage.py migrate
+python manage.py migrate
